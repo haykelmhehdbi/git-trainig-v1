23b1d1ea5c2c21d47d9ffe87a3eb5a35eb4067c7
haykel mhehdbi <thekinghaykel67@gmail.com>
2022-11-08 08:49:58 -0800

    added a new print statement from techdev

--- ex2-dfunction.py
+++ ex2-dfunction.py
@@ -1,5 +1,14 @@
 printf("the programe ran successfully")
 printf("first modifications for 2nd commit")
 priintf("added this line from the Github website for the first time ")
+<<<<<<< HEAD
 #---Addeed from the branch devstem for the first-------------------------------
 printf("added this line from the branch devteam2 from git terminal")
+=======
+
+printf("added this line of the code from devsteam1 from github ")
+>>>>>>> 4ba27faa4b0fb12c0dffba425902a0df14914c17
+
+
+print("added this line from techdev branch from git  terminal
+")
