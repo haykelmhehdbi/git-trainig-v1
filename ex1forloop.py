48b97d6227766278358ba4dae817860b2a3cf59b
haykel mhehdbi <thekinghaykel67@gmail.com>
2022-11-08 09:05:43 -0800

    added a new print statement from techdev 3

--- ex1forloop.py
+++ ex1forloop.py
@@ -2,3 +2,5 @@ printf("the programe ran successfully")
 #-----------------
 printf("added this line of the code from developer")
 printf("added this line of the code from developer by haykel")
+
+printf("added this line of the code from developer by haykel techdev")
