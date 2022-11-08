4ba27faa4b0fb12c0dffba425902a0df14914c17
haykelmhehdbi <84316940+haykelmhehdbi@users.noreply.github.com>
2022-11-08 00:29:35 -0800

    update ex1 file from devsteam1
    update ex1 file from devsteam1 for  the first time

--- ex2-dfunction.py
+++ ex2-dfunction.py
@@ -1,3 +1,5 @@
 printf("the programe ran successfully")
 printf("first modifications for 2nd commit")
 priintf("added this line from the Github website for the first time ")
+
+printf("added this line of the code from devsteam1 from github ")
