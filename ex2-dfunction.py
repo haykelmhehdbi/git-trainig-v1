539161087bacbec2386ba3c088211433d3672fae
haykel mhehdbi <thekinghaykel67@gmail.com>
2022-11-08 08:19:56 -0800

    Modified-ex2-branch-devteam2

--- ex2-dfunction.py
+++ ex2-dfunction.py
@@ -1,3 +1,5 @@
 printf("the programe ran successfully")
 printf("first modifications for 2nd commit")
 priintf("added this line from the Github website for the first time ")
+#---Addeed from the branch devstem for the first-------------------------------
+printf("added this line from the branch devteam2 from git terminal")
